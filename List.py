22301a2fda25ac67dbcda32dde1716d80b5a20ca
pradeeppvn <krishnarajpradeep@gmail.com>
2015-03-08 21:17:17 -0700

    Lists in python

--- List.py
+++ List.py
@@ -1,3 +1,22 @@
-a_list = [1,2,3,4,5,6,7,8]
 
+
+#####################
+#  List Operations 
+#####################
+a_list = [1,2,3,4,5,6,7,8]
 print a_list
+
+print 'Len(List) :' + str(len(a_list))
+print 'List[0] :' + str(a_list[0])
+
+print 'List[len(List)]  :' + str( a_list[ len(a_list)])
+
+#Insert items into list
+a_list.insert(0,'a')
+print a_list 
+
+#This is a comment #
+
+
+
+
