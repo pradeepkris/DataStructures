899b0b5ce16e25d1231864de8c8a15f3c2395f32
pradeeppvn <krishnarajpradeep@gmail.com>
2016-09-17 22:45:03 -0700

    Added Insert node functions to Linked List

--- Algorithms/LinkedList/Linked_List.py
+++ Algorithms/LinkedList/Linked_List.py
@@ -13,33 +13,46 @@ class Node(object):
     def set_next(self, new_next):
         self.next_node = new_next
 
-n1 = Node(10)
-n2 = Node(20)
-n3 = Node(30)
-
-n1.next_node = n2
-n2.next_node = n3
-
-
-
 class LinkedList(object):
 
     def __init__(self, head_node=None):
         self.head = head_node
         self.size = 1
 
-    # Insert Node at begining
-    def insert(self, data):
+    # Insert Node at begining, taking O(1) to add node
+    def insert_begin(self, data):
         new_node = Node(data)
-        new_node.set_next(self.head)
+        new_node.next_node = self.head
         self.head = new_node
 
-    # Delete Node at begining
-
     # Insert Node at end
+    def insert(self, data):
+        new_node = Node(data)
 
-    # Delete Node at end
+        if self.head is None:
+            self.head = new_node
+        else:
+            # find the last node, step responsible for O(n)
+            curr_node = self.head
+            while (curr_node.next_node):
+                curr_node = curr_node.next_node
+            curr_node.next_node = new_node
 
-    # Insert Node at Middle
 
-    # Delete Node at Middle
+    def print_list(self):
+        curr_node, out_text = self.head, ''
+        while(curr_node):
+            out_text += str(curr_node.data) + ' --> '
+            curr_node = curr_node.next_node
+
+        out_text += '*'
+        print out_text
+
+
+lst = LinkedList()
+lst.insert(10)
+lst.insert(20)
+lst.insert(30)
+lst.insert(5)
+lst.insert_begin(2)
+lst.print_list()
