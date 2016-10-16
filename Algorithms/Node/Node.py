122278b09f2ae12431dc6683b48bd531b4ea41c2
pradeeppvn <krishnarajpradeep@gmail.com>
2016-10-16 10:12:05 -0700

    Adding Node ...

---
+++ Algorithms/Node/Node.py
@@ -0,0 +1,8 @@
+#############################
+# NODE
+#############################
+class Node(object):
+
+    def __init__(self, data=None, next_node=None):
+        self.data = data
+        self.next_node = next_node
